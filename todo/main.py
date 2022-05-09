11b95de840218b34ae6f98a79ba402aef483aea4
arscool3 <45947743+arscool3@users.noreply.github.com>
2022-05-09 13:01:35 +0600

    implemented crud routes

--- todo/main.py
+++ todo/main.py
@@ -21,3 +21,27 @@ def get_db():
 @app.post("/todos/", response_model=schemas.Todo)
 def create_todo(todo: schemas.TodoCreate, db: Session = Depends(get_db)):
     return crud.create_todo(db, todo)
+
+
+@app.get("/todos/", response_model=list[schemas.Todo])
+def read_todos(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    todos = crud.get_todos(db, skip, limit)
+    return todos
+
+
+@app.get("/todos/{todo_id}", response_model=schemas.Todo)
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = crud.get_todo(db, todo_id)
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo item not found")
+    return db_todo
+
+
+@app.put("/todos/{todo_id}", response_model=schemas.Todo)
+def update_todo(todo_id: int, todo: schemas.TodoUpdate, db: Session = Depends(get_db)):
+    return crud.update_todo(db, todo_id, todo)
+
+
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id: int, db: Session = Depends(get_db)):
+    crud.delete_todo(db, todo_id=todo_id)
